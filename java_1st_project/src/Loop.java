11674e1c5b3d7c286355f9548e851476a60b3d47
zftyfaisalahmed <aaravkhan469@gmail.com>
2024-11-03 16:05:27 +0530

    Array, 2d and 3d

--- java_1st_project/src/Loop.java
+++ java_1st_project/src/Loop.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class Loop {
     public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("It was raining in Bengaluru");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the Value N : ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i + j == n / 2 || j - i == n / 2 || i - j == n / 2 || i + j == 15) {
+                    System.out.print("# ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
         }
     }
 }
